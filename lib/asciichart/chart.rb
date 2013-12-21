13211c1feddfd2832ff8bef2314b72cdaff80408
Jens Nazarenus <jens@0x6a.de>
2013-12-21 09:12:01 +0100

    chart.rb added documentation

--- lib/asciichart/chart.rb
+++ lib/asciichart/chart.rb
@@ -8,28 +8,45 @@ module Asciichart
       input =~ CHART_SYNTAX
     end
 
-    attr_accessor :width, :height
+    attr_reader :width, :height
 
+    # Initialize a new Chart with the given input
+    #
+    # input - the input chart
+    #
+    # Returns the new chart
     def initialize(input)
-      calc_width(input)
-      calc_height(input)
+      @width = calc_width(input)
+      @height = calc_height(input)
     end 
 
     def render
 
     end
 
+    # Calculates the width. The width is the length of the line with
+    # the most caharacters.
+    #
+    # input - the input which should be used for calculation
+    #
+    # Returns the width
     def calc_width(input)
-      @width = 0
+      width = 0
       input.each_line do |line|
-        if @width < line.length-1
-          @width = line.length-1
+        if width < line.length-1
+          width = line.length-1
         end
-      end  
+      end
+      widt
     end
 
+    # Calculates the height of the input.
+    #
+    # input - the input which should be used for calculation
+    #
+    # Returns the height
     def calc_height(input)
-      @height = input.lines.count 
+      input.lines.count 
     end
 
   end
