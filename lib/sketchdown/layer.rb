6db1b36e1bdc8e6aa32c175cfa8b64b5d71fb5df
Jens Nazarenus <jens@0x6a.de>
2013-12-24 13:12:00 +0100

    added method to create sublayers

--- lib/sketchdown/layer.rb
+++ lib/sketchdown/layer.rb
@@ -17,6 +17,13 @@ module Sketchdown
       @layer.push(cell)
     end
 
+    # Adds an array of cells to the layer
+    #
+    # cells - an array of cells
+    def add_all(cells)
+      @layer.concat(cells)
+    end
+
     # Removes a specified cell from the layer
     #
     # cell - the cell to remove
@@ -31,6 +38,96 @@ module Sketchdown
     # Returns true or false
     def include?(cell)
       @layer.include?(cell)
+    end
+
+    # Returns a sub layer which only contains cells
+    # in a specific deirection relative to the given cell.
+    #
+    # Example:
+    #
+    # cell: (1,0)
+    # direciton: east
+    #
+    #      (1,0)    (8,0)
+    #       v       v 
+    #      +--------+
+    # Returns (1,0), (2,0), ..., (8,0)
+    #
+    # cell: (0,4)
+    # direction: north
+    #
+    #      +
+    #      |
+    #      |
+    #      |
+    #      + < (0,4)
+    # Returns (0,4), (0,3), ..., (0,0)
+    #
+    # cell - the start cell
+    # direction - the direction
+    #
+    # Returns a sub layer with the given cells
+    def get_sublayer(cell, direction)
+      sublayer = Layer.new
+
+      if(direction == :east)
+        sublayer.add_all(get_cells_east_of(cell))
+      elsif(direction == :west)
+        sublayer.add_all(get_cells_west_of(cell))
+      elsif(direction == :south)
+        sublayer.add_all(get_cells_south_of(cell))
+      elsif(direction == :north)
+        sublayer.add_all(get_cells_north_of(cell))
+      end
+      sublayer
+    end
+
+    # Returns the cells north of the given cell on a given
+    # layer
+    def get_cells_north_of(layer, cell)
+      cells = Array.new
+      for e in layer
+        if e.is_north_of(cell) 
+          cells.push(e)
+        end
+      end
+      cells
+    end
+
+    # Returns the cells south of a given cell on a given
+    # layer
+    def get_cells_south_of(layer, cell)
+      cells = Array.new
+      for e in layer
+        if e.is_south_of(cell) 
+          cells.push(e)
+        end
+      end
+      cells
+    end
+
+    # Returns the cells west of a given cell on a given
+    # layer
+    def get_cells_west_of(layer, cell)
+      cells = Array.new
+      for e in layer
+        if e.is_west_of(cell) 
+          cells.push(e)
+        end
+      end
+      cells
+    end
+
+    # Returns the cells eastof a given cell on a given 
+    # layer
+    def get_cells_east_of(layer, cell)
+      cells = Array.new
+      for e in layer
+        if e.is_east_of(cell) 
+          cells.push(e)
+        end
+      end
+      cells
     end
 
     def_delegators :@layer, :each
--- lib/sketchdown/parser.rb
+++ lib/sketchdown/parser.rb
@@ -18,26 +18,26 @@ module Sketchdown
     # Returns all sub layer of a given root layer.
     #
     # Example:
-    # +----------+
-    # | +---+    |         +------+
-    # | |   |    | <------ |      |
-    # | +---+    |         +------+
-    # +----------+
+    #   +----------+
+    #   | +---+    |         +------+
+    #   | |   |    | <------ |      |
+    #   | +---+    |         +------+
+    #   +----------+
     #
     # layer 0:
-    # +----------+
-    # | +---+    |
-    # | |   |    |
-    # | +---+    |
-    # +----------+
+    #   +----------+
+    #   | +---+    |
+    #   | |   |    |
+    #   | +---+    |
+    #   +----------+
     #
     # layer 1:
-    # <------
+    #   <------
     #
     # layer 2:
-    # +------+
-    # |      |
-    # +------+
+    #   +------+
+    #   |      |
+    #   +------+
     #
     # layer - the layer used for calculation
     #
@@ -49,13 +49,52 @@ module Sketchdown
     # Checks if the given layer contains areas and return them in
     # an array.
     #
+    # Example:
+    #   +----------+
+    #   | +---+    |         +------+
+    #   | |   |    | <------ |      |
+    #   | +---+    |         +------+
+    #   +----------+
+    #
+    # area 0:
+    #   +----------+
+    #   | +---+    |
+    #   | |   |    |
+    #   | +---+    |
+    #   +----------+
+    #
+    # area 1:
+    #   +------+
+    #   |      |
+    #   +------+
+    #
     # layer - the given root layer used for calculation
     #
     # Returns a list of areas or an empty array if no areas found 
     def get_areas(layer)
-      areas = Array.new
 
-      
+    end
+
+    # Follow an arbitary figure until another figure is reached.
+    #
+    # Exmaple:
+    #
+    # from: [:line, :vertical]
+    # to: [:corner: north_east]
+    # direction: :east
+    #
+    # will follow the vertical line eastwards until a north east 
+    # corner is reached.
+    #
+    # layer - the layer for the calculcation
+    # from - an array which contains a mark and the type
+    # to - an array which contains a mark and the type
+    # direction - the direction where to follow, which may be
+    # :north, :east, :south or :west
+    #
+    # Returns an array of cells between from and to.
+    def follow_figure(layer, from, to, direction)
+
     end
 
   end
